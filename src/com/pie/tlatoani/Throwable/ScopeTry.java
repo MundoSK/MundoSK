db8726bb1b212f1a68753da0a5e176a561e52092
TlatoaniHJ <dannymittal@gmail.com>
2016-04-30 21:34:07 -0400

    Change the way it works

--- src/com/pie/tlatoani/Throwable/ScopeTry.java
+++ src/com/pie/tlatoani/Throwable/ScopeTry.java
@@ -30,7 +30,7 @@ public class ScopeTry extends CustomScope {
 				Mundo.debug(this, "TOString of going: " + going);
 				Mundo.debug(this, "Indent: " + going.getIndentation() + "MARK");
 				going = (TriggerItem) walkmethod.invoke(going, e);
-				if (going == null || going.getIndentation().length() <= indent) within = false;
+				if (going == null || going.getParent() != section) within = false;
 			} catch (Exception e1) {
 				within = false;
 				caught = e1;
