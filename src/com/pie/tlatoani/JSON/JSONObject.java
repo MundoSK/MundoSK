3b902b4f883a4cd73bfa702ef82a6e10fd92f7a6
TlatoaniHJ <dannymittal@gmail.com>
2016-05-06 17:51:46 -0400

    Add some code that got left out

--- src/com/pie/tlatoani/JSON/JSONObject.java
+++ src/com/pie/tlatoani/JSON/JSONObject.java
@@ -167,6 +167,61 @@ public class JSONObject {
     }
 
     /**
+     * Construct a JSONObject from a JSONTokener.
+     *
+     * @param x
+     *            A JSONTokener object containing the source string.
+     * @throws JSONException
+     *             If there is a syntax error in the source string or a
+     *             duplicated key.
+     */
+    public JSONObject(JSONTokener x) throws JSONException {
+        this();
+        char c;
+        String key;
+
+        if (x.nextClean() != '{') {
+            throw x.syntaxError("A JSONObject text must begin with '{'");
+        }
+        for (;;) {
+            c = x.nextClean();
+            switch (c) {
+                case 0:
+                    throw x.syntaxError("A JSONObject text must end with '}'");
+                case '}':
+                    return;
+                default:
+                    x.back();
+                    key = x.nextValue().toString();
+            }
+
+// The key is followed by ':'.
+
+            c = x.nextClean();
+            if (c != ':') {
+                throw x.syntaxError("Expected a ':' after a key");
+            }
+            this.putOnce(key, x.nextValue());
+
+// Pairs are separated by ','.
+
+            switch (x.nextClean()) {
+                case ';':
+                case ',':
+                    if (x.nextClean() == '}') {
+                        return;
+                    }
+                    x.back();
+                    break;
+                case '}':
+                    return;
+                default:
+                    throw x.syntaxError("Expected a ',' or '}'");
+            }
+        }
+    }
+
+    /**
      * Construct a JSONObject from a Map.
      *
      * @param map
