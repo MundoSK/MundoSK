8f158f67edddcc5fbf45699a4230284ebc9c6dfa
TlatoaniHJ <dannymittal@gmail.com>
2016-06-08 17:32:56 -0400

    Change method of variable setting

--- src/com/pie/tlatoani/Util/ScopeSaveCodeBlock.java
+++ src/com/pie/tlatoani/Util/ScopeSaveCodeBlock.java
@@ -5,6 +5,9 @@ import ch.njol.skript.classes.Changer;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.Variable;
+import ch.njol.skript.lang.VariableString;
+import ch.njol.skript.util.StringMode;
+import ch.njol.skript.variables.Variables;
 import ch.njol.util.Kleenean;
 import com.pie.tlatoani.Mundo;
 import org.bukkit.event.Event;
@@ -15,13 +18,17 @@ import javax.annotation.Nullable;
  * Created by Tlatoani on 6/5/16.
  */
 public class ScopeSaveCodeBlock extends CustomScope {
-    private Variable variable;
+    private VariableString variableString;
+    private Boolean variableIsLocal;
     private SkriptCodeBlock skriptCodeBlock;
 
     @Override
     public boolean init(Expression<?>[] exprs, int arg1, Kleenean arg2, SkriptParser.ParseResult arg3) {
         if (exprs[0] instanceof Variable) {
-            variable = (Variable) exprs[0];
+            Variable variable = (Variable) exprs[0];
+            String origstring = variable.isLocal() ? variable.toString().substring(2, variable.toString().length() - 1) : variable.toString().substring(1, variable.toString().length() - 1);
+            variableString = VariableString.newInstance(origstring, StringMode.VARIABLE_NAME);
+            variableIsLocal = variable.isLocal();
             return true;
         }
         Skript.error(exprs[0].toString() + " is not a variable!");
@@ -41,6 +48,6 @@ public class ScopeSaveCodeBlock extends CustomScope {
 
     @Override
     public void go(Event e) {
-        variable.change(e, new Object[]{skriptCodeBlock}, Changer.ChangeMode.SET);
+        Variables.setVariable(variableString.toString(e), skriptCodeBlock, e, variableIsLocal);
     }
 }
