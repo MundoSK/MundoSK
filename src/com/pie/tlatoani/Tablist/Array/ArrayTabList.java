a912a9bfea4dc446591eb4695722c1fb0953590a
TlatoaniHJ <dannymittal@gmail.com>
2016-07-24 17:28:44 -0400

    debug UUID

--- src/com/pie/tlatoani/Tablist/Array/ArrayTabList.java
+++ src/com/pie/tlatoani/Tablist/Array/ArrayTabList.java
@@ -43,6 +43,7 @@ public class ArrayTabList {
         WrappedChatComponent chatComponent = WrappedChatComponent.fromJson(TabListManager.colorStringToJson(displayName));
         //UUID uuid = UUID.nameUUIDFromBytes(("MundoSKTabList::" + column + "," + (row < 10 ? "0" + row : row)).getBytes(TabListManager.utf8));
         UUID uuid = UUID.nameUUIDFromBytes(new byte[]{new Integer((column * 20) + row).byteValue()});
+        Mundo.debug(this, "UUID generated for " + column + ", " + row + ": " + uuid.toString());
         UUID head = heads[column - 1][row - 1];
         WrappedGameProfile gameProfile = new WrappedGameProfile(uuid, "");
         if (head != null) {
