5d63af48e1ddd04e346d23b6b1756ee9d9132793
TlatoaniHJ <dannymittal@gmail.com>
2017-08-16 19:03:46 -0400

    1.7.5-BETA.9.33

--- src/com/pie/tlatoani/Book/BookMundo.java
+++ src/com/pie/tlatoani/Book/BookMundo.java
@@ -12,10 +12,9 @@ public class BookMundo {
 
     public static void load() {
         ListUtil.registerTransformer(TransBookPages.class, "itemstack", "page");
-        Registration.registerExpression(ExprBook.class,ItemStack.class, ExpressionType.COMBINED,"%itemstack% titled %string%, [written] by %string%, [with] pages %strings%");
-        Registration.registerExpression(ExprTitleOfBook.class,String.class,ExpressionType.PROPERTY,"title of %itemstack%");
-        //Registration.registerExpression(ExprAuthorOfBook.class,String.class,ExpressionType.PROPERTY,"author of %itemstack%");
-        Registration.registerPropertyExpression(ExprNewAuthorOfBook.class, String.class, "itemstacks", "author");
+        Registration.registerExpression(ExprBook.class, ItemStack.class, ExpressionType.COMBINED,"[written] book titled %string%[,] [written] by %string%[,] [with] pages %strings%");
+        Registration.registerPropertyExpression(ExprTitleOfBook.class, String.class, "itemstacks", "title");
+        Registration.registerPropertyExpression(ExprAuthorOfBook.class, String.class, "itemstacks", "author");
 
     }
 }
--- src/com/pie/tlatoani/Book/ExprBook.java
+++ src/com/pie/tlatoani/Book/ExprBook.java
@@ -1,64 +1,51 @@
 package com.pie.tlatoani.Book;
 
-import java.util.List;
-
-import javax.annotation.Nullable;
-
-import org.bukkit.event.Event;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
-
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
+import org.bukkit.Material;
+import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 
 public class ExprBook extends SimpleExpression<ItemStack>{
-	private Expression<ItemStack> book;
 	private Expression<String> title;
 	private Expression<String> author;
 	private Expression<String> texts;
 
 	@Override
 	public Class<? extends ItemStack> getReturnType() {
-		// TODO Auto-generated method stub
 		return ItemStack.class;
 	}
 
 	@Override
 	public boolean isSingle() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public boolean init(Expression<?>[] expr, int matchedPattern, Kleenean arg2, ParseResult arg3) {
-		// TODO Auto-generated method stub
-		book = (Expression<ItemStack>) expr[0];
-		title = (Expression<String>) expr[1];
-		author = (Expression<String>) expr[2];
-		texts = (Expression<String>) expr[3];
+		title = (Expression<String>) expr[0];
+		author = (Expression<String>) expr[1];
+		texts = (Expression<String>) expr[2];
 		return true;
 	}
 
 	@Override
-	public String toString(@Nullable Event arg0, boolean arg1) {
-		// TODO Auto-generated method stub
-		return "border length of world";
+	public String toString(Event event, boolean arg1) {
+		return "written book titled " + title + " written by " + author + " with pages " + texts;
 	}
 
 	@Override
-	@Nullable
-	protected ItemStack[] get(Event arg0) {
-		ItemStack input = book.getSingle(arg0);
-		BookMeta meta = (BookMeta) input.getItemMeta();
-		meta.setTitle(title.getSingle(arg0));
-		meta.setAuthor(author.getSingle(arg0));
-		meta.setPages(texts.getArray(arg0));
-		ItemStack result = book.getSingle(arg0).clone();
-		result.setItemMeta(meta);
-		return new ItemStack[]{result};
+	protected ItemStack[] get(Event event) {
+		ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+		BookMeta meta = (BookMeta) book.getItemMeta();
+		meta.setTitle(title.getSingle(event));
+		meta.setAuthor(author.getSingle(event));
+		meta.setPages(texts.getArray(event));
+		book.setItemMeta(meta);
+		return new ItemStack[]{book};
 	}
 
 }
