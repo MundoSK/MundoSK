59ff5614f1c6e89d47288a69666e6f36cc3c3863
TlatoaniHJ <dannymittal@gmail.com>
2017-08-18 11:33:13 -0400

    1.7.5-BETA.9.38

--- src/com/pie/tlatoani/Throwable/ThrowableMundo.java
+++ src/com/pie/tlatoani/Throwable/ThrowableMundo.java
@@ -1,6 +1,7 @@
 package com.pie.tlatoani.Throwable;
 
 import ch.njol.skript.lang.ExpressionType;
+import com.pie.tlatoani.Util.MundoPropertyExpression;
 import com.pie.tlatoani.Util.Registration;
 
 /**
@@ -14,14 +15,44 @@ public class ThrowableMundo {
         Registration.registerScope(ScopeTry.class, "try");
         Registration.registerScope(ScopeCatch.class, "catch in %object%");
         Registration.registerEffect(EffPrintStackTrace.class, "print stack trace of %throwable%");
-        /*Registration.registerExpression(OldExprCause.class,Throwable.class, ExpressionType.PROPERTY,"throwable cause of %throwable%", "%throwable%'s throwable cause");
-        Registration.registerExpression(OldExprDetails.class,String.class,ExpressionType.PROPERTY,"details of %throwable%", "%throwable%'s details");
-        Registration.registerExpression(OldExprPropertyNameOfSTE.class,String.class,ExpressionType.PROPERTY,"(0¦class|1¦file|2¦method) name of %stacktraceelement%", "%stacktraceelement%'s (0¦class|1¦file|2¦method) name");
-        Registration.registerExpression(OldExprLineNumberOfSTE.class,Integer.class,ExpressionType.PROPERTY,"line number of %stacktraceelement%", "%stacktraceelement%'s line number");*/
         Registration.registerPropertyExpression(ExprCause.class, Throwable.class, "throwable", "throwable cause");
         Registration.registerPropertyExpression(ExprDetails.class, String.class, "throwable", "details");
         Registration.registerPropertyExpression(ExprPropertyNameOfSTE.class, String.class, "stacktraceelement", "class name", "file name", "method name");
         Registration.registerPropertyExpression(ExprLineNumberOfSTE.class, Number.class, "stacktraceelement", "line number");
         Registration.registerExpression(ExprStackTrace.class,StackTraceElement.class,ExpressionType.PROPERTY,"stack trace of %throwable%", "%throwable%'s stack trace");
     }
+
+    public static class ExprCause extends MundoPropertyExpression<Throwable, Throwable> {
+        @Override
+        public Throwable convert(Throwable throwable) {
+            return throwable.getCause();
+        }
+    }
+
+    public static class ExprDetails extends MundoPropertyExpression<Throwable, String> {
+        @Override
+        public String convert(Throwable throwable) {
+            return throwable.getMessage();
+        }
+    }
+
+    public static class ExprLineNumberOfSTE extends MundoPropertyExpression<StackTraceElement, Number> {
+        @Override
+        public Number convert(StackTraceElement stackTraceElement) {
+            return stackTraceElement.getLineNumber();
+        }
+    }
+
+    public static class ExprPropertyNameOfSTE extends MundoPropertyExpression<StackTraceElement, String> {
+        @Override
+        public String convert(StackTraceElement stackTraceElement) {
+            switch (property) {
+                case "class name": return stackTraceElement.getClassName();
+                case "file name": return stackTraceElement.getFileName();
+                case "method name": return stackTraceElement.getMethodName();
+            };
+            throw new IllegalStateException("Invalid property: " + property);
+        }
+    }
+
 }
