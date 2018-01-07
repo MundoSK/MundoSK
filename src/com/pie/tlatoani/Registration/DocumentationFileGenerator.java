f294f6bd9369bb356ed79cb524e57e23384ffd8e
TlatoaniHJ <dannymittal@gmail.com>
2018-01-07 16:26:51 -0500

    1.7.5-BETA.10.93
    Fixes the documentation file generator to include the message about
    scopes.

--- src/com/pie/tlatoani/Registration/DocumentationFileGenerator.java
+++ src/com/pie/tlatoani/Registration/DocumentationFileGenerator.java
@@ -64,7 +64,7 @@ public class DocumentationFileGenerator {
         if (docElem instanceof Scope) {
             descJSON.set(0, "Not an effect, but rather a scope (written with a colon at the end with a section of indented code under it, like an if statement or loop). ");
         }
-        result.put("description", fromStringArray(docElem.description));
+        result.put("description", descJSON);
         result.put("patterns", fromStringArray(docElem.syntaxes));
         if (docElem instanceof Type) {
             Type typeDocElem = (Type) docElem;
