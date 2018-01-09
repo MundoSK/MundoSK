b77e3e53a299a599bf67196f62b7fa90a5b84c52
TlatoaniHJ <dannymittal@gmail.com>
2018-01-08 23:20:04 -0500

    1.8.2-BETA.2
    Continue testing packet info aliases

--- src/com/pie/tlatoani/ProtocolLib/Alias/PacketInfoAlias.java
+++ src/com/pie/tlatoani/ProtocolLib/Alias/PacketInfoAlias.java
@@ -3,13 +3,17 @@ package com.pie.tlatoani.ProtocolLib.Alias;
 import ch.njol.skript.ScriptLoader;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.log.ParseLogHandler;
+import ch.njol.skript.log.SkriptLogger;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 import com.pie.tlatoani.ProtocolLib.PacketManager;
 import com.pie.tlatoani.Util.BaseEvent;
 import com.pie.tlatoani.Util.Logging;
+import com.pie.tlatoani.Util.Reflection;
 import org.bukkit.event.Event;
 
+import java.util.Iterator;
 import java.util.Optional;
 
 /**
@@ -20,6 +24,8 @@ public class PacketInfoAlias {
     public final String alias;
     public final String original;
     public final Expression<?> expression;
+
+    public static final Reflection.MethodInvoker SKRIPT_PARSER_PARSE = Reflection.getMethod(SkriptParser.class, "parse", Iterator.class);
 
     public static class ContainerEvent extends BaseEvent {
         public final PacketContainer packet;
@@ -37,18 +43,40 @@ public class PacketInfoAlias {
     }
 
     public static Optional<PacketInfoAlias> create(PacketType packetType, String syntax, String original) {
-        String resultSyntax = original.replace("%packet%", "{_packet}");
+        String resultSyntax = original.replace("%packet%", "event-packet");
         String currentEventName = ScriptLoader.getCurrentEventName();
         Class<? extends Event>[] currentEvents = ScriptLoader.getCurrentEvents();
         ScriptLoader.setCurrentEvent("ExprPacketInfoAliasPacketEvent", ContainerEvent.class);
         Logging.debug(PacketInfoAlias.class, "packetType = " + packetType + ", syntax = " + syntax + ", original = " + original + ", resultSyntax = " + resultSyntax);
-        Expression<?> expression = SkriptParser.parseStatic(resultSyntax, PacketManager.packetInfoExpressionInfoIterator(), "'" + original + "' is not a valid packet info expression");
+        //Expression<?> expression = SkriptParser.parseStatic(resultSyntax, PacketManager.packetInfoExpressionInfoIterator(), "'" + original + "' is not a valid packet info expression");
+        Expression<?> expression = parsePacketInfoExpression(resultSyntax, "'" + original + "' is not a valid packet info expression");
         Logging.debug(PacketInfoAlias.class, "expr = " + expression);
         ScriptLoader.setCurrentEvent(currentEventName, currentEvents);
         if (expression == null) {
             return Optional.empty();
         }
         return Optional.of(new PacketInfoAlias(packetType, syntax, original, expression));
+    }
+
+    //Method adapted from the parseStatic() method of SkriptParser in Skript
+    private static Expression<?> parsePacketInfoExpression(String syntax, String defaultError) {
+        final ParseLogHandler log = SkriptLogger.startParseLogHandler();
+        final Expression<?> result;
+        try {
+            SkriptParser parser = new SkriptParser(syntax, SkriptParser.ALL_FLAGS);
+            result = (Expression<?>) SKRIPT_PARSER_PARSE.invoke(parser, PacketManager.packetInfoExpressionInfoIterator());
+            if (result != null) {
+                log.printLog();
+                return result;
+            }
+            log.printError(defaultError);
+            return null;
+        } catch (Exception e) {
+            Logging.reportException(PacketInfoAlias.class, e);
+        } finally {
+            log.stop();
+        }
+        return null;
     }
 
     public String toString(Expression<PacketContainer> packetExpression, Event event, boolean b) {
