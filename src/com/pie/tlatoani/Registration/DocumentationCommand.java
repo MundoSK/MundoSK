51de97eb3d4c65292cd42925d573ef106723c443
TlatoaniHJ <dannymittal@gmail.com>
2018-02-17 14:51:35 -0500

    1.8.5-BETA.8
    Fix documentation search

--- src/com/pie/tlatoani/Registration/DocumentationCommand.java
+++ src/com/pie/tlatoani/Registration/DocumentationCommand.java
@@ -25,6 +25,7 @@ public class DocumentationCommand {
         for (List<DocumentationElement> docElems : Documentation.getAllElements().getAllGroups()) {
             Logging.debug(Documentation.class, "Searching through " + docElems);
             Optional<DocumentationElement> docElemOptional = MundoUtil.binarySearchCeiling(docElems, docElemName, (name, docElem) -> Documentation.WORD_BY_WORD_COMPARATOR.compare(name, docElem.name));
+            Logging.debug(DocumentationCommand.class, "Found docElem " + docElemOptional);
             if (docElemOptional.filter(docElem -> MundoUtil.wordsStartWith(docElem.name.toLowerCase(), docElemName)).isPresent()) {
                 docElemOptional.get().display(sender);
                 return;
