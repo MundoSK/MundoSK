6fc5681d2e484f477d27692624323f966abe9d3e
TlatoaniHJ <dannymittal@gmail.com>
2018-06-25 23:59:07 -0400

    1.8.6-BETA.27

--- src/com/pie/tlatoani/ZExperimental/TestEventConstruction.java
+++ src/com/pie/tlatoani/ZExperimental/TestEventConstruction.java
@@ -7,6 +7,7 @@ import com.pie.tlatoani.Core.Static.Reflection;
 import org.bukkit.ChatColor;
 import org.bukkit.event.Event;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
@@ -45,13 +46,34 @@ public class TestEventConstruction {
         for (Constructor<?> constructor : eventClass.getConstructors()) {
             try {
                 Object[] parameters = new Object[constructor.getParameterCount()];
+                Class<?>[] paramTypes = constructor.getParameterTypes();
+                for (int i = 0; i < parameters.length; i++) {
+                    Class<?> paramType = paramTypes[i];
+                    if (paramType == int.class || paramType == Integer.class || paramType == Number.class) {
+                        parameters[i] = (int) 0;
+                    } else if (paramType == long.class || paramType == Integer.class) {
+                        parameters[i] = (long) 0;
+                    } else if (paramType == double.class || paramType == Double.class) {
+                        parameters[i] = (double) 0;
+                    } else if (paramType == short.class || paramType == Short.class) {
+                        parameters[i] = (short) 0;
+                    } else if (paramType == float.class || paramType == Float.class) {
+                        parameters[i] = (float) 0;
+                    } else if (paramType == byte.class || paramType == Byte.class) {
+                        parameters[i] = (byte) 0;
+                    } else if (paramType == String.class) {
+                        parameters[i] = "abcd";
+                    } else if (paramType.isArray()) {
+                        parameters[i] = Array.newInstance(paramType.getComponentType(), 1);
+                    }
+                }
                 Object creation = constructor.newInstance(parameters);
                 StringJoiner logBuilder = new StringJoiner(
                         ", ",
                         ChatColor.DARK_GREEN + "Success, creation: " + creation + ", constructor: " + eventClass.getName() + "(",
                         ")"
                 );
-                for (Class<?> paramType : constructor.getParameterTypes()) {
+                for (Class<?> paramType : paramTypes) {
                     logBuilder.add(paramType.getName());
                 }
                 Logging.info(logBuilder.toString());
