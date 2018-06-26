595fbedb1fe0ce15d0723a2b1714030b97727f96
TlatoaniHJ <dannymittal@gmail.com>
2018-06-25 23:08:16 -0400

    1.8.6-BETA.26
    Test event construction.

---
+++ src/com/pie/tlatoani/ZExperimental/TestEventConstruction.java
@@ -0,0 +1,63 @@
+package com.pie.tlatoani.ZExperimental;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.SkriptEventInfo;
+import com.pie.tlatoani.Core.Static.Logging;
+import com.pie.tlatoani.Core.Static.Reflection;
+import org.bukkit.event.Event;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringJoiner;
+
+public class TestEventConstruction {
+
+    public static void test() {
+        try {
+            Collection<SkriptEventInfo<?>> skriptEvents =
+                    (Collection<SkriptEventInfo<?>>) Reflection.getStaticField(Skript.class, "events");
+            Set<Class<? extends Event>> successfulEventClasses = new HashSet<>();
+            Set<Class<? extends Event>> failedEventClasses = new HashSet<>();
+            for (SkriptEventInfo<?> skriptEventInfo : skriptEvents) {
+                for (Class<? extends Event> eventClass : skriptEventInfo.events) {
+                    if (!successfulEventClasses.contains(eventClass) && !failedEventClasses.contains(eventClass)) {
+                        if (testEventClass(eventClass)) {
+                            successfulEventClasses.add(eventClass);
+                        } else {
+                            failedEventClasses.add(eventClass);
+                        }
+                    }
+                }
+            }
+            Logging.info("Out of " + (successfulEventClasses.size() + failedEventClasses.size()) + " event classes,");
+            Logging.info(successfulEventClasses.size() + " succeeded and " + failedEventClasses.size() + " failed.");
+            Logging.info("The failures were " + failedEventClasses);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            Logging.reportException(TestEventConstruction.class, e);
+        }
+    }
+
+    private static boolean testEventClass(Class<? extends Event> eventClass) {
+        for (Constructor<?> constructor : eventClass.getConstructors()) {
+            try {
+                Object[] parameters = new Object[constructor.getParameterCount()];
+                Object creation = constructor.newInstance(parameters);
+                StringJoiner logBuilder = new StringJoiner(
+                        ", ",
+                        "Success, creation: " + creation + ", constructor: " + eventClass.getName() + "(",
+                        ")"
+                );
+                for (Class<?> paramType : constructor.getParameterTypes()) {
+                    logBuilder.add(paramType.getName());
+                }
+                Logging.info(logBuilder.toString());
+                return true;
+            } catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {}
+        }
+        Logging.info("Failure for class " + eventClass.getName());
+        return false;
+    }
+}
--- src/com/pie/tlatoani/ZExperimental/ZExperimentalMundo.java
+++ src/com/pie/tlatoani/ZExperimental/ZExperimentalMundo.java
@@ -1,6 +1,7 @@
 package com.pie.tlatoani.ZExperimental;
 
 import com.pie.tlatoani.Core.Registration.Registration;
+import com.pie.tlatoani.Core.Static.Scheduling;
 
 /**
  * Created by Tlatoani on 8/8/17.
@@ -9,5 +10,6 @@ public class ZExperimentalMundo {
 
     public static void load() {
         Registration.registerEvent("Custom Element Event", CustomEffect.class, CustomElementEvent.class, "[light_jsoup] [new] [custom] effect %string%");
+        Scheduling.syncDelay(1, TestEventConstruction::test);
     }
 }
