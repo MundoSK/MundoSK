4deb4643aa558744ce5b5c55565d6a866cd72845
TlatoaniHJ <dannymittal@gmail.com>
2018-08-24 13:11:42 -0400

    1.8.6-BETA.55
    Remove the TestEventConstruction class due to errors caused and lack of
    feasability.

--- src/com/pie/tlatoani/ZExperimental/ZExperimentalMundo.java
+++ src/com/pie/tlatoani/ZExperimental/ZExperimentalMundo.java
@@ -10,6 +10,6 @@ public class ZExperimentalMundo {
 
     public static void load() {
         Registration.registerEvent("Custom Element Event", CustomEffect.class, CustomElementEvent.class, "[light_jsoup] [new] [custom] effect %string%");
-        Scheduling.syncDelay(1, TestEventConstruction::test);
+        //Scheduling.syncDelay(1, TestEventConstruction::test);
     }
 }
